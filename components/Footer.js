bc1142f5bdf18e864b70aa2f61933778dba1cf8d
Curtis Ruder HP <curtisjruder@gmail.com>
2025-09-13 16:09:35 -0500

    other changes

--- components/Footer.js
+++ components/Footer.js
@@ -12,7 +12,8 @@ class Footer extends HTMLElement{
         <footer>
           <ul>
             <li><a href="#Home">Home</a></li>
-            <li><a href="#About">About</a></li>                    
+            <li><a href="#About">About</a></li>
+            <li><a href="#Services">Services</a></li>                               
             <li><a href="#Contact">Contact</a></li>
           </ul>
           <ul class="social-row">            
